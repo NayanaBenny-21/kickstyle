74a0068b645e7f05f720b848b96a5761f3cfbd38
NayanaBenny-21 <nayanabenny21@gmail.com>
2026-02-04 13:30:31 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
- require("dotenv").config();
-
+require("dotenv").config();
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URL);
-        console.log("DB conneted");
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
+};
 
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-        process.exit(1);
-    }
-    mongoose.connection.on('connected', () => console.log('Mongoose default connection open'));
-mongoose.connection.on('disconnected', () => console.log('Mongoose default connection disconnected'));
-
-}
+// optional listeners (fine to keep)
+mongoose.connection.on("connected", () =>
+  console.log("Mongoose default connection open")
+);
+mongoose.connection.on("disconnected", () =>
+  console.log("Mongoose default connection disconnected")
+);
 
 module.exports = connectDB;
